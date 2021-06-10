6f227929bfbc2bf1a102c9fd7c7d7fd9252c2787
TurtleWXG <45228312+TurtleWXG@users.noreply.github.com>
2021-06-10 10:04:47 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import WeChatCollection from '../views/WeChatCollection.vue'
+import DataVisualization from '../views/DataVisualization.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'WeChatCollection',
-    component: WeChatCollection
+    name: 'DataVisualization',
+    component: DataVisualization
   },
   {
     path: '/about',
---
+++ src/views/DataVisualization.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="view">
+    <div class="view-header">停车场收费</div>
+    <div class="view-amount"></div>
+    <van-search v-model="value" shape="round" background="#4fc08d" placeholder="请输入搜索关键词" />
+    <van-button type="primary">主要按钮</van-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataVisualization'
+}
+</script>
+<style lang="scss" scoped>
+//
+</style>
